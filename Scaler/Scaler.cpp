39a7fb1d1d414448a978525bfd47112abfa9df75
Vladithur <ybargatin@gmail.com>
2018-06-10 00:36:31 -0700

    Commit

--- Scaler/Scaler.cpp
+++ Scaler/Scaler.cpp
@@ -10,10 +10,15 @@ void Scaler::bmp_to_bytes(const char* file_path, uint16_t width, uint16_t height
 	*destination = readBmp(file_path, width, height);
 }
 
+Image Scaler::scale(Image src, float factor)
+{
+	return Image();
+}
+
 Image Scaler::readBmp(const char* file_path, uint16_t width, uint16_t height) {
 	FILE *f;
 	fopen_s(&f, file_path, "rb");
-	uint8_t * buffer = new uint8_t[width * height * 4];
+	uint8_t * buffer = new uint8_t[width * height * 3];
 	fseek(f, 54, SEEK_SET);
 	fread(buffer, width * (height) * 4 * 4, 1, f);
 	fclose(f);
--- Scaler/Scaler.h
+++ Scaler/Scaler.h
@@ -14,4 +14,6 @@ private:
 public:
 	static Image bmp_to_bytes(const char* file_path, uint16_t width, uint16_t height);
 	static void bmp_to_bytes(const char* file_path, uint16_t width, uint16_t height, Image* destination);
+
+	static Image scale(Image src, float factor);
 };
